e65492e1f0de7fe502ab5e61fc9f3ed89041aeb4
priest4612 <prie4612@gmail.com>
2021-05-13 01:05:54 +0300

    Изменил код в соответствии с замечаниями

--- homework.py
+++ homework.py
@@ -1,4 +1,5 @@
 import datetime as dt
+from typing import Union
 
 
 class Calculator:
@@ -12,32 +13,38 @@ class Calculator:
 
     def __init__(self, limit):
         self.limit = limit
-        self.records = list()
+        self.records = []
 
     def add_record(self, record: 'Record') -> None:
+        '''Сохраняет новую запись о расходах
+        record -> принимает экземпляр класса Record
+        '''
         self.records.append(record)
 
-    def get_today_stats(self, date: str = '') -> float:
-        total = 0
+    def get_today_stats(self, date: Union[str, dt.date] = '') -> float:
+        '''Рассчитывает, сколько денег потрачено за указанную дату
+        date -> принимает дату в формате "str" или "dt.date"
+        '''
         moment = date
-        if (date == ''):
-            moment = dt.datetime.now().date()
-        today_records = list(filter(lambda record: record.date == moment,
-                             self.records))
 
-        for record in today_records:
-            total = total + record.amount
+        if date == '':
+            moment = dt.date.today()
 
-        return total
+        return sum([rec.amount for rec in self.records if rec.date == moment])
+
+    def get_balance_today(self) -> float:
+        '''Рассчитывает остаток на сегодня'''
+        return self.limit - self.get_today_stats()
 
     def get_week_stats(self):
+        '''Рассчитывает расходы за неделю'''
         long_week = 7
         week_total = 0
-        today = dt.datetime.now().date()
+        today = dt.datetime.today()
+
         for day in range(long_week):
             moment = today - dt.timedelta(days=day)
-            print(moment)
-            week_total = week_total + self.get_today_stats(moment)
+            week_total = week_total + self.get_today_stats(moment.date())
 
         return week_total
 
@@ -45,20 +52,12 @@ class Calculator:
 class CashCalculator(Calculator):
     '''
     Калькулятор денег.
-    add_record() - Сохраняет новую запись о расходах
-    get_today_stats() - Считает, сколько денег потрачено сегодня
-    get_week_stats() - Считает, сколько денег потрачено за последние 7 дней
-    get_today_cash_remained(currency) - Определяет, сколько ещё денег можно
+    get_today_cash_remained(currency) - Рассчитывает, сколько ещё денег можно
     потратить сегодня в рублях, долларах или евро
     '''
 
-    def __init__(self, limit):
-        super().__init__(limit)
-
-    # Ревьюер если ты это читаешь, то ты меня поймешь. Это ЖЕСТЬ!!! Это
-    # целенавравленный выстрел в ногу, который дал кретический промах в голову
-    EURO_RATE = 89.78
-    USD_RATE = 73.78
+    EURO_RATE: float = 89.78
+    USD_RATE: float = 73.78
 
     def get_today_cash_remained(self, currency: str = 'rub') -> str:
         '''
@@ -66,46 +65,44 @@ class CashCalculator(Calculator):
         одну из строк "rub", "usd" или "eur"
         '''
         currencies = {
-            'rub': {'name': 'руб', 'rate': 1.00},
-            'usd': {'name': 'USD', 'rate': self.USD_RATE},
-            'eur': {'name': 'Euro', 'rate': self.EURO_RATE}
+            'rub': ['руб', 1.00],
+            'usd': ['USD', self.USD_RATE],
+            'eur': ['Euro', self.EURO_RATE]
         }
 
-        current_currency = currencies[currency]
-        current_balance = round(((self.limit - self.get_today_stats())
-                                 / current_currency["rate"]), 2)
+        if currency not in currencies.keys():
+            return f'Мне неизвестна данная валюта {currency}'
 
-        if (current_balance > 0):
+        cur_name, cur_rate = currencies[currency]
+
+        current_balance = round(self.get_balance_today() / cur_rate, 2)
+
+        if current_balance > 0 :
             return (f'На сегодня осталось {current_balance} '
-                    f'{current_currency["name"]}')
+                    f'{cur_name}')
 
-        elif (current_balance == 0):
+        if current_balance == 0 :
             return 'Денег нет, держись'
 
-        else:
-            return (f'Денег нет, держись: твой долг - {abs(current_balance)} '
-                    f'{current_currency["name"]}')
+        return (f'Денег нет, держись: твой долг - {abs(current_balance)} '
+                f'{cur_name}')
 
 
 class CaloriesCalculator(Calculator):
     '''
     Калькулятор калорий
-    add_record() - Сохраняет новую запись о расходах
-    get_today_stats() - Считает, сколько денег потрачено сегодня
-    get_week_stats() - Считает, сколько денег потрачено за последние 7 дней
-    get_calories_remained() - Определять, сколько ещё калорий можно/нужно
+    get_calories_remained() - Рассчитывает, сколько ещё калорий можно/нужно
     получить сегодня
     '''
 
     def get_calories_remained(self):
-        current_balance = self.limit - self.get_today_stats()
+        current_balance = self.get_balance_today()
 
         if (current_balance > 0):
             return (f'Сегодня можно съесть что-нибудь ещё, но с общей '
                     f'калорийностью не более {current_balance} кКал')
 
-        else:
-            return 'Хватит есть!'
+        return 'Хватит есть!'
 
 
 class Record:
@@ -119,32 +116,35 @@ class Record:
     '''
     amount: float
     comment: str
-    date: str
+    date: Union[str, dt.datetime]
 
-    def __init__(self, amount, comment, date='') -> None:
+    def __init__(self, amount, comment, date: Union[str, dt.datetime] ='') -> None:
         self.amount = amount
         self.comment = comment
         self.date = self.convert_date(date)
 
-    def convert_date(self, date: str) -> str:
+    def convert_date(self, date: Union[str, dt.date]) -> dt.date:
+        if date == '' :
+            return dt.date.today()
+
         date_format = '%d.%m.%Y'
-        if (date == ''):
-            return dt.datetime.now().date()
-        else:
-            return dt.datetime.strptime(date, date_format).date()
+        return dt.datetime.strptime(date, date_format).date()
 
+if __name__ == '__main__':
 
-cash_calculator = CashCalculator(1000)
+    cash_calculator = CashCalculator(1000)
+    # дата в параметрах не указана,
+    # так что по умолчанию к записи
+    # должна автоматически добавиться сегодняшняя дата
+    cash_calculator.add_record(Record(amount=145, comment='кофе'))
+    # и к этой записи тоже дата должна добавиться автоматически
+    cash_calculator.add_record(Record(amount=300, comment='Серёге за обед'))
+    # а тут пользователь указал дату, сохраняем её
+    cash_calculator.add_record(Record(amount=3000,
+                                    comment='бар в Танин др',
+                                    date='08.11.2019'))
 
-# дата в параметрах не указана,
-# так что по умолчанию к записи
-# должна автоматически добавиться сегодняшняя дата
-cash_calculator.add_record(Record(amount=145, comment='кофе'))
-# и к этой записи тоже дата должна добавиться автоматически
-cash_calculator.add_record(Record(amount=300, comment='Серёге за обед'))
-# а тут пользователь указал дату, сохраняем её
-cash_calculator.add_record(Record(amount=3000,
-                                  comment='бар в Танин др',
-                                  date='08.11.2019'))
-
-print(cash_calculator.get_today_cash_remained('rub'))
+    print(cash_calculator.get_today_cash_remained('rub'))
+    print(cash_calculator.get_today_cash_remained('eur'))
+    print(cash_calculator.get_today_cash_remained('usd'))
+    print(cash_calculator.get_today_cash_remained('tug'))
